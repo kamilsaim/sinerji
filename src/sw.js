1d8e051e9f6eb36386f896934a4bdefc0133da12
Kamil <saimkamil@gmail.com>
2026-08-23 01:57:58 +0300

    feat: PWA manifest ve service worker ekle

---
+++ src/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sinerji-shell-v1';
+const SHELL_FILES = ['/', '/index.html', '/manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached ?? fetch(event.request))
+  );
+});

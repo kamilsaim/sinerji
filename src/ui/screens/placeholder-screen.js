9cabd3de9fcc352b9feacffdd7641ad51d4352a3
Kamil Saim <saimkamil@gmail.com>
2026-08-24 16:15:27 +0300

    chore: repo içeriğini kaldır, sadece README kalsın (yayın Firebase'den yapılıyor, kod artık yerelde tutuluyor)

--- src/ui/screens/placeholder-screen.js
+++
@@ -1,8 +0,0 @@
-export function renderPlaceholderScreen(container, label) {
-  container.innerHTML = `
-    <div class="placeholder-screen">
-      <p class="placeholder-screen__label">${label}</p>
-      <p class="placeholder-screen__text">Bu ekran yakında burada olacak ✦</p>
-    </div>
-  `;
-}
--- test/smoke.test.js
+++
@@ -1,9 +0,0 @@
-// test/smoke.test.js
-import { describe, it, expect } from 'vitest';
-import { APP_NAME } from '../src/lib/constants.js';
-
-describe('proje iskeleti', () => {
-  it('APP_NAME sabitini dışa aktarır', () => {
-    expect(APP_NAME).toBe('Sinerji');
-  });
-});
